51d318235c7714aa03f86aa233eeba7c982bf9ca
Haider Ali <wahabw259@gmail.com>
2024-10-04 05:22:12 +0500

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+function Card () {
+    return (
+        <>
+            <div className="card">
+                <img className="card-image" alt="Profile Picture" src="https://via.placeholder.com/150"></img>
+                <h2 className="card-title">Haider Ali</h2>
+                <p className="card-text">I am a Software Engineer</p>
+            </div>
+        </>
+    )
+}
+
+export default Card;
